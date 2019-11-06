e2e123a735483a188581a5f5b5fae63c95358d5e
Leon Dario Arango Amaya <breakermoob@hotmail.com>
2019-11-06 00:01:27 -0500

    Configs

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config ={
-   mongoURL: process.env.DB_URL || 'mongodb+srv://db_user:EkIK16LEiF1goDVF@cluster0-ceujf.mongodb.net/db_message',
+   mongoURL: process.env.DB_URL || 'mongodb+srv://db_user:{p455_Only4Me:D}@cluster0-ceujf.mongodb.net/db_message',
    port: parseInt(process.env.PORT, 10) || 3000,
    host:process.env.HOST || 'http://localhost:',
    publicRoute:process.env.PUBLIC_ROUTE || '/app',
